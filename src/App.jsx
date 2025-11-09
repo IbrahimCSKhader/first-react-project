f674b56bb2a5df1ed3784f72f565527edd7c5876
IbrahimCSKhader <ik2907951@gmail.com>
2025-11-09 22:47:42 +0200

    add the spa type for links

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,40 @@
-import 'bootstrap/dist/css/bootstrap.min.css'
-import '@fortawesome/fontawesome-free/css/all.min.css'
-import React from 'react'
-import Navbar from './components/navbar/navbar.jsx'
-import Hero from './components/hero/hero.jsx'
-import Portfolio from './components/portfolio/portfolio.jsx'
-import About from './components/about/about.jsx'
-import ContactMe from './components/contactme/contactme.jsx'
-import Footer from './components/footer/footer.jsx'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import '@fortawesome/fontawesome-free/css/all.min.css';
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Navbar from './components/navbar/navbar.jsx';
+import Hero from './components/hero/hero.jsx';
+import Portfolio from './components/portfolio/portfolio.jsx';
+import About from './components/about/about.jsx';
+import ContactMe from './components/contactme/contactme.jsx';
+import Footer from './components/footer/footer.jsx';
 
-export default function App() {
-	return (
-		<div className="App">
-			<Navbar />
-			<Hero />
-			<Portfolio />
-			<About />
-			<ContactMe />
-			<Footer />
-		</div>
-	)
+
+function Home() {
+  return (
+    <>
+      <Hero />
+      <Portfolio />
+      <About />
+      <ContactMe />
+    </>
+  );
 }
 
+export default function App() {
+  return (
+      <div className="App">
+        <Navbar />
+
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/hero" element={<Hero />} />
+          <Route path="/portfolio" element={<Portfolio />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<ContactMe />} />
+        </Routes>
+
+        <Footer />
+      </div>
+  );
+}
--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -1,18 +1,21 @@
-import React from 'react'
-import styles from './navbar.module.css'
+import React from 'react';
+import styles from './navbar.module.css';
+import { Link } from 'react-router-dom';
 
 export default function Navbar() {
-	return (
-		<nav className={`${styles.container} d-flex justify-content-between align-items-center text-white px-5 py-5`}>
-			
-			<h1 className="fw-bold m-0">START BOOTSTRAP</h1>
+  return (
+    <nav className={`${styles.container} d-flex justify-content-between align-items-center text-white px-5 py-5`}>
+    <Link to="/" className="text-decoration-none text-white">      <h1 className="fw-bold m-0">START BOOTSTRAP</h1>
+</Link>
 
-			<div className="d-flex gap-4 fw-semibold align-items-center">
-				<a href="#!" className="text-decoration-none text-white">PORTFOLIO</a>
-				<a href="#!" className="text-decoration-none text-white">ABOUT</a>
-				<a href="#!" className="text-decoration-none text-white">CONTACT</a>
-			</div>
-			
-		</nav>
-	)
+
+      <div className="d-flex gap-4 fw-semibold align-items-center">
+        <Link to="/portfolio" className="text-decoration-none text-white">PORTFOLIO</Link>
+        <Link to="/hero" className="text-decoration-none text-white">HERO</Link>
+        <Link to="/about" className="text-decoration-none text-white">ABOUT</Link>
+        <Link to="/contact" className="text-decoration-none text-white">CONTACT</Link>
+      </div>
+      
+    </nav>
+  );
 }
